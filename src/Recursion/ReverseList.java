9e3346bf8a3457cadeb9568b3dfaed0c64eac137
AdarshJaiswal <adarsh.jaiswal18@gmail.com>
2025-11-05 21:52:03 +0530

    message

---
+++ src/Recursion/ReverseList.java
@@ -0,0 +1,40 @@
+package Recursion;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class ReverseList {
+
+    static Node reverse(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node newHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        head = reverse(head);
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
---
+++ src/Recursion/countZero.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+public class countZero {
+    static int countZero(int n) {
+        if (n == 0) {
+            return 1;}
+        return helper(n, 0);
+    }
+    static int helper(int n, int count) {
+        if (n == 0) {
+            return count;
+        }
+        if (n % 10 == 0) {
+            count++;
+        }
+        return helper(n / 10, count);
+    }
+    public static void main(String[] args) {
+        int num = 102030;
+        System.out.println(countZero(num));
+    }
+}
+
